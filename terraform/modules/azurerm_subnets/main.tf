79c4a103a0c99870cc6667149408209bc4fea494
ferhad_Baaqieel <ferhad.baaqieel@gmail.com>
2025-10-29 01:12:50 +0300

    define multiple subnet resources

--- terraform/modules/azurerm_subnets/main.tf
+++ terraform/modules/azurerm_subnets/main.tf
@@ -1,7 +1,15 @@
-resource "azurerm_subnet" "subnet" {
-  name                 = var.name
+# define multiple subnet resources instead of one, to not a good practice but just for testing
+
+resource "azurerm_subnet" "db_subnet" {
+  name                 = "db-subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.vnet_name
-  address_prefixes     = var.address_prefixes
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.vnet_name
+  address_prefixes     = ["10.0.1.0/24"]
+}
