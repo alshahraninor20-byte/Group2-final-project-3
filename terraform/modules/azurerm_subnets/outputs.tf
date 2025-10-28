4a89ae97cb652b5af5a1f2848c6291583c6d38c1
ferhad_Baaqieel <ferhad.baaqieel@gmail.com>
2025-10-29 01:17:00 +0300

    update subnet module outputs

--- terraform/modules/azurerm_subnets/outputs.tf
+++ terraform/modules/azurerm_subnets/outputs.tf
@@ -1,7 +1,7 @@
-output "subnet" {
-  value = azurerm_subnet.subnet
-}
-
-output "id" {
-  value = azurerm_subnet.subnet.id
+# change output from a single resource output to a grouped/mapped output
+output "subnets" {
+  value = {
+    db_subnet  = azurerm_subnet.db_subnet.id
+    aks_subnet = azurerm_subnet.aks_subnet.id
+  }
 }
--- terraform/modules/azurerm_subnets/variables.tf
+++ terraform/modules/azurerm_subnets/variables.tf
@@ -1,16 +1,7 @@
-variable "name" {
-  type = string
-} 
-
 variable "resource_group_name" {
   type = string
 }
 
 variable "vnet_name" {
   type = string
-}
-
-
-variable "address_prefixes" {
-    default     = []
 }
