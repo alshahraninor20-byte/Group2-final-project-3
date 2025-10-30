d1ab627b3b260b63415372295e6da98ba84cf91d
Ahmed Basuwaiteen <ahmadsa38@hotmail.com>
2025-10-30 12:25:53 +0300

    fix vnet module

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ module "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = local.location
   address_space       = local.address_space
-  name = local.vnet_name
+  vnet_name           = local.vnet_name
 }
 
 module "subnets" {
